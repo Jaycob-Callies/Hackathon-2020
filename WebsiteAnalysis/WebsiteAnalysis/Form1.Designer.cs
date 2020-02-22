a038707e7d448b1151594dce49d3008dbdf53750
jcallies <jcallies@eecs.wsu.edu>
2020-02-22 12:51:06 -0800

    Added more Skeleton Code for Node system, included HTML Agility Pack

--- WebsiteAnalysis/WebsiteAnalysis/Form1.Designer.cs
+++ WebsiteAnalysis/WebsiteAnalysis/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
             this.SuspendLayout();
             // 
             // textBox1
@@ -43,17 +44,25 @@
             // 
             this.button1.Location = new System.Drawing.Point(118, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(75, 20);
             this.button1.TabIndex = 1;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(12, 41);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(776, 397);
+            this.treeView1.TabIndex = 3;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.treeView1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
@@ -67,6 +76,7 @@
 
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TreeView treeView1;
     }
 }
 
--- WebsiteAnalysis/WebsiteAnalysis/Form1.cs
+++ WebsiteAnalysis/WebsiteAnalysis/Form1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
@@ -22,14 +23,109 @@ namespace WebsiteAnalysis
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)//Main Start button Click Behavior
         {
-            System.Drawing.SolidBrush myBrush = new System.Drawing.SolidBrush(System.Drawing.Color.Red);
-            System.Drawing.Graphics formGraphics;
-            formGraphics = this.CreateGraphics();
-            formGraphics.FillEllipse(myBrush, new Rectangle(0, 0, 200, 300));
-            myBrush.Dispose();
-            formGraphics.Dispose();
+            //System.Drawing.SolidBrush myBrush = new System.Drawing.SolidBrush(System.Drawing.Color.Red);//Temp example code
+            //System.Drawing.Graphics formGraphics;////////////////////////////////////
+            //formGraphics = this.CreateGraphics();
+            //formGraphics.FillEllipse(myBrush, new Rectangle(0, 0, 200, 300));
+            //myBrush.Dispose();
+            //formGraphics.Dispose();
+            // Populates a TreeView control with example nodes. 
+            treeView1.Visible = true;
+            treeView1.BringToFront();
+            treeView1.CreateGraphics();
+                treeView1.BeginUpdate();
+                treeView1.Nodes.Add("Parent");
+                treeView1.Nodes[0].Nodes.Add("Child 1");
+                treeView1.Nodes[0].Nodes.Add("Child 2");
+                treeView1.Nodes[0].Nodes[1].Nodes.Add("Grandchild");
+                treeView1.Nodes[0].Nodes[1].Nodes[0].Nodes.Add("Great Grandchild");
+                treeView1.EndUpdate();
+            treeView1.ExpandAll();
+            treeView1.Show();
+
         }
     }
+
+    public class URLNode : TreeNode// class to store data of website links
+    {
+        public string URL;//this url
+
+        URLNode(string initializedURL)//create tree with URL declared
+        {
+            ///////////////
+        }
+
+        void AddParsed()//get data from website about links
+        {
+            //////////////////
+        }
+
+    }
+
+    public class URLTree : TreeView
+    {
+
+        URLTree(URLNode rootNode)
+        {
+            //////////////////////////
+        }
+
+        private void FillMyTreeView()
+        {
+            //Filled With Example Code
+
+            //// Add customers to the ArrayList of Customer objects.
+            //this.Nodes.Add(URLNode);
+            //for (int x = 0; x < 1000; x++)
+            //{
+            //    tempURLNode.Add(new Customer("Customer" + x.ToString()));
+            //}
+
+            //// Add orders to each Customer object in the ArrayList.
+            //foreach (Customer customer1 in tempURLNode)
+            //{
+            //    for (int y = 0; y < 15; y++)
+            //    {
+            //        customer1.CustomerOrders.Add(new Order("Order" + y.ToString()));
+            //    }
+            //}
+
+            //// Display a wait cursor while the TreeNodes are being created.
+            //Cursor.Current = new Cursor("MyWait.cur");
+
+            //// Suppress repainting the TreeView until all the objects have been created.
+            //treeView1.BeginUpdate();
+
+            //// Clear the TreeView each time the method is called.
+            //treeView1.Nodes.Clear();
+
+            //// Add a root TreeNode for each Customer object in the ArrayList.
+            //foreach (Customer customer2 in tempURLNode)
+            //{
+            //    treeView1.Nodes.Add(new TreeNode(customer2.CustomerName));
+
+            //    // Add a child treenode for each Order object in the current Customer object.
+            //    foreach (Order order1 in customer2.CustomerOrders)
+            //    {
+            //        treeView1.Nodes[tempURLNode.IndexOf(customer2)].Nodes.Add(
+            //          new TreeNode(customer2.CustomerName + "." + order1.OrderID));
+            //    }
+            //}
+
+            //// Reset the cursor to the default for all controls.
+            //Cursor.Current = Cursors.Default;
+
+            //// Begin repainting the TreeView.
+            //treeView1.EndUpdate();
+        }
+    }
+
+    // Create a new ArrayList to hold the Customer objects.
+    //private ArrayList tempURLNode = new ArrayList(); 
+
+
+
+
 }
